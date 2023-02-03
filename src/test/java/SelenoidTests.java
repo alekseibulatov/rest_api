7fdea26393eb894331b1e60b8b187ee42eca736d
alekseibulatov <59173362+alekseibulatov@users.noreply.github.com>
2023-02-03 14:06:17 +0300

    add checkTotalWithGiven, checkTotalWithLogs methods

--- src/test/java/SelenoidTests.java
+++ src/test/java/SelenoidTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class SelenoidTests {
@@ -23,6 +24,27 @@ public class SelenoidTests {
     public void checkTotalWithStatus() {
         get("https://selenoid.autotests.cloud/status")
                 .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    public void checkTotalWithGiven() {
+        given()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .statusCode(200)
+                .body("total", is(20));
+    }
+
+    @Test
+    public void checkTotalWithLogs() {
+        given()
+                .log().all()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
                 .statusCode(200)
                 .body("total", is(20));
     }
