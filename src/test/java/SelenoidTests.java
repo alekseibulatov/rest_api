7ed6fbe22a35ed854df4e2320f9347a3634aa6f1
alekseibulatov <59173362+alekseibulatov@users.noreply.github.com>
2023-02-03 13:33:34 +0300

    add Rest Assured to build.gradle

--- src/test/java/SelenoidTests.java
+++ src/test/java/SelenoidTests.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 public class SelenoidTests {
+     /*
+        1. Make request to https://selenoid.autotests.cloud/status
+        2. Get response { total: 20, used: 0, queued: 0, pending: 0, browsers: { android: { 8.1: { } },
+            chrome: { 100.0: { }, 99.0: { } }, chrome-mobile: { 86.0: { } }, firefox: { 97.0: { }, 98.0: { } },
+            opera: { 84.0: { }, 85.0: { } } } }
+        3. Check total is 20
+     */
+
+    @Test
+    public void checkTotal() {
+
+    }
+
+
 }
